1eebe574497266ec69975716a403a73a3e0da812
Laye Yomba Ifono <layeyomba12@gmail.com>
2024-11-19 16:35:07 +0000

    Mise à jour

--- heritage/heritage/src/Bicycle.java
+++ heritage/heritage/src/Bicycle.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class Bicycle {
+    // La class Bicycle a deux champs
+    public int gear; // Embrayage
+    public int speed; // Vitesse
 
-public class Bicyle {
+    // constructor
+    public Bicycle(int gear, int speed) {
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    // Methode pour appliquer le freinnage
+    public  void applyBreak( int decrement){
+        speed -= decrement;
+    }
+
+    // Methode pour accélération
+    public void setSpeed( int increment){
+        speed += increment;
+    }
+
+    // Méthode toString() pour imprimer les informations du vélo
+    @Override
+    public String toString() {
+        return ("Le nombre d'engrenages est : " + gear + "\n" +
+                "La vitesse du vélo est : " + speed
+
+                );
+    }
 }
--- heritage/heritage/src/MountainBike.java
+++ heritage/heritage/src/MountainBike.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
 
-public class MountainBike {
+public class MountainBike extends Bicycle {
+
+    // La sous-classe MountainBike ajoute un champ supplémentaire
+    public int seatHeight; // Hauteur de siège
+
+    // Constructor
+   public  MountainBike(int gear, int speed, int startHeight){
+       super(gear, speed);
+       seatHeight = startHeight;
+   }
+
+   // Méthode suplémentaire de la sous-classe MountainBite
+    public void setHeight(int newValue){
+       seatHeight = newValue;
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString() + "\nLa hauteur est : " + seatHeight);
+    }
 }
